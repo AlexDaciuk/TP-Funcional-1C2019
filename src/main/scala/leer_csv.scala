5685f9d4585b95b84871bf016263cf8b2ad24b07
Alexis Daciuk <adaciuk@gmail.com>
2019-05-31 17:30:37 -0300

    Lo leo de una forma funciona, creo

--- src/main/scala/leer_csv.scala
+++ src/main/scala/leer_csv.scala
@@ -1,13 +1,26 @@
 import java.io.File
+import cats.effect._
 import kantan.csv._ // All kantan.csv types.
 import kantan.csv.ops._ // Enriches types with useful methods.
+import kantan.codecs.resource.ResourceIterator
 
-object leerCSV extends App{
+
+object leerCSV extends App {
+
   val data = new File("assets/csv/train.csv")
 
   val reader = data.asCsvReader[List[String]](rfc.withHeader)
 
-  val fila = List[String](reader.next())
+  // def cargar_db(reader: ResourceIterator[List[String]], session: Resource[IO, SessionIO]): Either[String, String] = {
+  def iterador(lista: ReadResult[List[String]]) : Either[String, String] ={
+    println("Entre")
+    lista match {
+      case Right(l) => { println(l)
+                         iterador(reader.next())}
+      case Left(k) => Left("Termino el archivo")
+    }
+  }
 
+  iterador(reader.next())
 
 }
