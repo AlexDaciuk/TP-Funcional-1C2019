326bbb08283ab521058d3df687cc42cd3f362778
Alex Daciuk <adaciuk@gmail.com>
2019-06-07 01:53:12 -0300

    Subo lo ultimo que hice en clase

--- src/main/scala/leer_csv.scala
+++ src/main/scala/leer_csv.scala
@@ -228,7 +228,7 @@ object leerCSV extends App {
       trip_distance: Float
   )
 
-  def procesar_primera_parte(list: List[String]): PrimeraParte = {
+  def procesar_primera_parte(lista: List[String]): PrimeraParte = {
     val slice1 = lista.slice(0, 21)
     val primeraParte = PrimeraParte(
       slice1(0).toInt,
@@ -253,6 +253,7 @@ object leerCSV extends App {
       slice1(19),
       slice1(20).toInt
     )
+    primeraParte
   }
 
   def procesar_segunda_parte(lista: List[String]): SegundaParte = {
@@ -280,6 +281,8 @@ object leerCSV extends App {
       slice2(19).toInt,
       slice2(20).toInt
     )
+
+    segundaParte
   }
 
   def procesar_tercera_parte(lista: List[String]): TerceraParte = {
@@ -290,16 +293,16 @@ object leerCSV extends App {
       slice3(2),
       slice3(3),
       slice3(4).toInt,
-      slice3(5).toInt,
-      slice3(6).toInt,
-      slice3(7).toInt,
+      slice3(5),
+      slice3(6),
+      slice3(7),
       slice3(8).toInt,
-      slice3(9),
-      slice3(10),
-      slice3(11),
+      slice3(9).toInt,
+      slice3(10).toInt,
+      slice3(11).toInt,
       slice3(12),
-      slice3(13).toInt,
-      slice3(14).toInt,
+      slice3(13),
+      slice3(14),
       slice3(15),
       slice3(16).toInt,
       slice3(17).toInt,
@@ -308,17 +311,19 @@ object leerCSV extends App {
       slice3(20).toInt,
       slice3(21)
     )
+
+    terceraParte
   }
 
   def procesar_cuarta_parte(lista: List[String]): CuartaParte = {
     val slice4 = lista.slice(64, 86)
     val cuarteParte = CuartaParte(
       slice4(0),
-      slice4(1).toInt,
+      slice4(1),
       slice4(2).toInt,
-      slice4(3).toFloat,
+      slice4(3).toInt,
       slice4(4).toFloat,
-      slice4(5).toInt,
+      slice4(5).toFloat,
       slice4(6).toInt,
       slice4(7).toInt,
       slice4(8).toInt,
@@ -333,11 +338,14 @@ object leerCSV extends App {
       slice4(17).toInt,
       slice4(18).toInt,
       slice4(19).toInt,
-      slice4(20).toInt
+      slice4(20).toInt,
+      slice4(21)
     )
+
+    cuarteParte
   }
 
-  def procesar_quinta_parte(lista: Lista[String]): QuintaParte = {
+  def procesar_quinta_parte(lista: List[String]): QuintaParte = {
     val slice5 = lista.slice(86, 92)
     val quintaParte = QuintaParte(
       slice5(0).toInt,
@@ -348,9 +356,215 @@ object leerCSV extends App {
       slice5(5).toFloat,
       slice5(6).toFloat
     )
+
+    quintaParte
   }
 
-  def cargar_db(lista: List[String]): Either[String, Unit] = {}
+  def insertar(primeraParte: PrimeraParte,
+               segundaParte: SegundaParte,
+               terceraParte: TerceraParte,
+               cuartaParte: CuartaParte,
+               quintaParte: QuintaParte): Update0 =
+    sql"""
+    insert
+    	into
+    		funcional( maiScore,
+    		deviceMatch,
+    		factorCodes,
+    		firstEncounter,
+    		icAddress,
+    		icernet,
+    		icSuspicious,
+    		icVelocity,
+    		icIdentity,
+    		ipRoutingMethod,
+    		reasonCode,
+    		timeOnPage,
+    		billinCountryCode,
+    		cancelled,
+    		cardContryCode,
+    		pp_1,
+    		pp_30,
+    		pp_60,
+    		pp_90,
+    		caseDate,
+    		caseMinuteDistance,
+    		casesCount,
+    		channel,
+    		correlID,
+    		countDifferentCards,
+    		countDifferentInstallments,
+    		countryCode,
+    		countryFrom,
+    		countryTo,
+    		distanceToArrival,
+    		distanceToDeparture,
+    		domainProc,
+    		maiAdvice,
+    		maiVerification,
+    		maiReason,
+    		maiRisk,
+    		maibisScore,
+    		maiStatus,
+    		maiUnique,
+    		maiAvgSecs,
+    		maiBuys,
+    		maySearches,
+    		eulerBadge,
+    		maiPax,
+    		maiType,
+    		maiRels,
+    		maiApp,
+    		mai_urgency,
+    		mai_network,
+    		mai_all_pax,
+    		maiLastSeconds,
+    		apocrypha,
+    		friendly,
+    		hours_since_last_verification,
+    		iataFrom,
+    		iataTo,
+    		ip_city,
+    		mai_language,
+    		mai_negative,
+    		mai_suspect,
+    		mai_os,
+    		mai_policy_score,
+    		mai_pulevel,
+    		maibis_reason,
+    		mai_city,
+    		mai_region,
+    		maitris_score,
+    		lag_time_hours,
+    		many_holders_for_card,
+    		many_name_for_document,
+    		online_airport_state,
+    		online_billing_address,
+    		online_cep_number_bond,
+    		online_city_bond,
+    		online_ddd,
+    		online_ddd_bond,
+    		online_death,
+    		online_email,
+    		online_family_bond,
+    		online_ip_state,
+    		online_name,
+    		online_code,
+    		online_phone,
+    		online_queries,
+    		online_state_bond,
+    		payment_card_type,
+    		payment_installments,
+    		same_field_features,
+    		speed_to_departure,
+    		total_usd_amounts,
+    		triangulation_height,
+    		triangulation_height_speed,
+    		trip_distance )
+    	values ( ${primeraParte.maiScore},
+        ${primeraParte.deviceMatch},
+        ${primeraParte.factorCodes},
+        ${primeraParte.firstEncounter},
+        ${primeraParte.icAddress},
+        ${primeraParte.icInternet},
+        ${primeraParte.icSuspicious},
+        ${primeraParte.icVelocity},
+        ${primeraParte.icIdentity},
+        ${primeraParte.ipRoutingMethod},
+        ${primeraParte.reasonCode},
+        ${primeraParte.timeOnPage},
+        ${primeraParte.billinCountryCode},
+        ${primeraParte.cancelled},
+        ${primeraParte.cardContryCode},
+        ${primeraParte.pp_1},
+        ${primeraParte.pp_30},
+        ${primeraParte.pp_60},
+        ${primeraParte.pp_90},
+        ${primeraParte.caseDate},
+        ${primeraParte.caseMinuteDistance},
+        ${segundaParte.casesCount},
+        ${segundaParte.channel},
+        ${segundaParte.correlID},
+        ${segundaParte.countDifferentCards},
+        ${segundaParte.countDifferentInstallments},
+        ${segundaParte.countryCode},
+        ${segundaParte.countryFrom},
+        ${segundaParte.countryTo},
+        ${segundaParte.distanceToArrival},
+        ${segundaParte.distanceToDeparture},
+        ${segundaParte.domainProc},
+        ${segundaParte.maiAdvice},
+        ${segundaParte.maiVerification},
+        ${segundaParte.maiReason},
+        ${segundaParte.maiRisk},
+        ${segundaParte.maibisScore},
+        ${segundaParte.maiStatus},
+        ${segundaParte.maiUnique},
+        ${segundaParte.maiAvgSecs},
+        ${segundaParte.maiBuys},
+        ${segundaParte.maySearches},
+        ${terceraParte.eulerBadge},
+        ${terceraParte.maiPax},
+        ${terceraParte.maiType},
+        ${terceraParte.maiRels},
+        ${terceraParte.maiApp},
+        ${terceraParte.mai_urgency},
+        ${terceraParte.mai_network},
+        ${terceraParte.mai_all_pax},
+        ${terceraParte.maiLastSeconds},
+        ${terceraParte.apocrypha},
+        ${terceraParte.friendly},
+        ${terceraParte.hours_since_last_verification},
+        ${terceraParte.iataFrom},
+        ${terceraParte.iataTo},
+        ${terceraParte.ip_city},
+        ${terceraParte.mai_language},
+        ${terceraParte.mai_negative},
+        ${terceraParte.mai_suspect},
+        ${terceraParte.mai_os},
+        ${terceraParte.mai_policy_score},
+        ${terceraParte.mai_pulevel},
+        ${terceraParte.maibis_reason},
+        ${cuartaParte.mai_city},
+        ${cuartaParte.mai_region},
+        ${cuartaParte.maitris_score},
+        ${cuartaParte.lag_time_hours},
+        ${cuartaParte.many_holders_for_card},
+        ${cuartaParte.many_name_for_document},
+        ${cuartaParte.online_airport_state},
+        ${cuartaParte.online_billing_address},
+        ${cuartaParte.online_cep_number_bond},
+        ${cuartaParte.online_city_bond},
+        ${cuartaParte.online_ddd},
+        ${cuartaParte.online_ddd_bond},
+        ${cuartaParte.online_death},
+        ${cuartaParte.online_email},
+        ${cuartaParte.online_family_bond},
+        ${cuartaParte.online_ip_state},
+        ${cuartaParte.online_name},
+        ${cuartaParte.online_code},
+        ${cuartaParte.online_phone},
+        ${cuartaParte.online_queries},
+        ${cuartaParte.online_state_bond},
+        ${cuartaParte.payment_card_type},
+        ${quintaParte.payment_installments},
+        ${quintaParte.same_field_features},
+        ${quintaParte.speed_to_departure},
+        ${quintaParte.total_usd_amounts},
+        ${quintaParte.triangulation_height},
+        ${quintaParte.triangulation_height_speed},
+        ${quintaParte.trip_distance}
+     )
+    """.update
+
+
+  def cargar_db(lista: List[String]): Either[String, Unit] = {
+    insertar(procesar_primera_parte(lista),
+             procesar_segunda_parte(lista),
+             procesar_tercera_parte(lista),
+             procesar_cuarta_parte(lista),
+             procesar_quinta_parte(lista))
+  }
 
   def iterador(lista: ReadResult[List[String]]): Either[String, String] = {
     println("Entre")
@@ -365,6 +579,6 @@ object leerCSV extends App {
     }
   }
 
-//  iterador(reader.next())
+  iterador(reader.next())
 
 }
