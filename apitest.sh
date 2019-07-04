2aeb9f611deb316ef676e48be9642ee606e33044
Alexis Daciuk <adaciuk@gmail.com>
2019-07-04 09:33:02 -0300

    Cambio el nombre de start para que refleje que es un shell script y creo el archivo que compila y ejecuta el test de la ap

---
+++ apitest.sh
@@ -0,0 +1,6 @@
+# Compilo, creo el container y corro la test de la api con el otro CSV
+cd api-test
+sbt compile
+sbt docker:publishLocal
+cd ..
+docker-compose -f docker-compose-apitest.yml up apitest
--- start.sh
+++ start.sh
@@ -23,10 +23,3 @@ sbt compile
 sbt docker:publishLocal
 cd ..
 docker-compose -f docker-compose-rest-service.yml up rest-service
-
-# Compilo, creo el container y corro la test de la api con el otro CSV
-#cd api-test
-#sbt compile
-#sbt docker:publishLocal
-#cd ..
-#docker-compose -f docker-compose-apitest.yml up apitest
